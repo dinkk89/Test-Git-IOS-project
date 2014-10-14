07ce403fc303639913000127c3a51455899ee838
Dinesh Kakumani <dinkk89@gmail.com>
2014-10-13 23:44:03 -0700

    added a test class

---
+++ CCtest.h
@@ -0,0 +1,13 @@
+//
+//  CCtest.h
+//  Git Test
+//
+//  Created by Dinesh Kakumani on 10/13/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCtest : NSObject
+
+@end
---
+++ CCtest.m
@@ -0,0 +1,13 @@
+//
+//  CCtest.m
+//  Git Test
+//
+//  Created by Dinesh Kakumani on 10/13/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import "CCtest.h"
+
+@implementation CCtest
+
+@end
